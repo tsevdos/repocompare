eb61f9e458bd3cc891551ac319b36ca786a8e06b
John Tsevdos <tsevdosjohn@gmail.com>
2016-12-28 17:17:12 +0200

    Update webpack config to use ES6. Seperate and optimize development and production configurations

---
+++ webpack.config.babel.js
@@ -0,0 +1,82 @@
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+
+// Set BABEL_ENV to development or production
+const LAUNCH_COMMAND = process.env.npm_lifecycle_event;
+const isProduction = LAUNCH_COMMAND === 'build';
+process.env.BABEL_ENV = isProduction ? 'production' : 'development';
+
+// Set up PATHS constants
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'dist'),
+};
+
+// Plugins
+// Create an HTML page on root
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: PATHS.app + '/index.html',
+  filename: '../index.html',
+  inject: 'body'
+});
+
+// More optimization on bundle.js in production
+const productionPlugin = new webpack.DefinePlugin({
+  'process.env': {
+    NODE_ENV: JSON.stringify('production')
+  }
+});
+
+const baseConfig = {
+  entry: [
+    PATHS.app
+  ],
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js',
+    publicPath: "dist/"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.js$/, /\.jsx$/],
+        include: PATHS.app,
+        loader: "babel"
+      },
+      {
+        test: /\.json$/,
+        include: PATHS.app,
+        loader: "json"
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      }
+    ]
+  },
+  resolve: {
+    // root: path.resolve('./app'), // TODO: Enable it and fix paths
+    extensions: ['', '.js', '.jsx', '.json', '.scss'],
+    modulesDirectories: ['node_modules']
+  }
+};
+
+const developmentConfig = {
+  devtool: 'cheap-module-inline-source-map',
+  devServer: {
+    hot: true,
+    inline: true,
+    progress: true,
+    port: 8080,
+    historyApiFallback: true
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
+
+const productionConfig = {
+  devtool: 'cheap-module-source-map',
+  plugins: [HTMLWebpackPluginConfig, productionPlugin]
+};
+
+export default Object.assign({}, baseConfig, isProduction === true ? productionConfig : developmentConfig);
