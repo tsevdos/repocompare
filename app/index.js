f70febd127b0eb027be85f997c5c86b529bea4a5
John Tsevdos <tsevdosjohn@gmail.com>
2016-12-30 04:32:57 +0200

    Better seperation of application initialization and routes

--- app/index.js
+++ app/index.js
@@ -1,4 +1,16 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react';
+import repoStore from 'stores/RepoStore';
+import Repo from 'stores/Repo';
 import routers from './config/routes';
 
-ReactDOM.render(routers, document.getElementById('app'));
+const bootstrap = new Repo({ username: 'twbs', reponame: 'bootstrap' });
+repoStore.addRepo(bootstrap);
+
+ReactDOM.render(
+  <Provider repoStore={repoStore}>
+    {routers}
+  </Provider>,
+  document.getElementById('app')
+);
