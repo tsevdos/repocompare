8f3332737d6b669f875371d398b811a00cb5662f
John Tsevdos <tsevdosjohn@gmail.com>
2016-12-31 21:12:10 +0200

    Styled Footer component

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { footer } from './Footer.css';
 
 const Footer = () =>
 (
-  <footer className="footer">
+  <footer className={footer}>
     <div className="container">
       <p className="text-muted">
         Like this project? Then feel to <a target="_blank" rel="noopener noreferrer" href="https://github.com/tsevdos/repocompare">contribute</a> or
