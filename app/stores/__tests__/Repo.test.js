4f0cdbc217d4f801d5c6279ba6eaa83d4c938327
John Tsevdos <tsevdosjohn@gmail.com>
2017-01-04 00:44:15 +0200

    Unit tests for RepoStore (#23)
    * Testing infastructure ready (Jest)
    
    * RepoStore unit test
    
    * Simplified lint fixing script on package.json
    
    * Stores unit testing

---
+++ app/stores/__tests__/Repo.test.js
@@ -0,0 +1,44 @@
+import Repo from '../Repo';
+
+describe('Repo', () => {
+  beforeEach(function() {
+    this.bootstrap = new Repo(
+      {
+        username: 'twbs',
+        reponame: 'bootstrap'
+      }
+    );
+  });
+
+  it('must have correct attributes', function() {
+    expect(this.bootstrap.id).toEqual('twbs/bootstrap');
+    expect(this.bootstrap.username).toBe('twbs');
+    expect(this.bootstrap.reponame).toBe('bootstrap');
+    expect(this.bootstrap.isFetching).toBe(true);
+    expect(this.bootstrap.animate).toBe(false);
+    expect(this.bootstrap.data).toBe(null);
+    expect(this.bootstrap.repoNameFull).toBe('twbs/bootstrap');
+  });
+
+  it('#fetchData', function () {
+    jest.useFakeTimers();
+    const fetchData = jest.fn().mockImplementation(() => {
+      setTimeout(() => { // Fake API rsponse
+        this.bootstrap.isFetching = false;
+        this.bootstrap.data = {};
+      }, 1000);
+    });
+    fetchData();
+    jest.runAllTimers();
+
+    expect(this.bootstrap.isFetching).toEqual(false);
+    expect(this.bootstrap.data).toEqual({});
+  });
+
+  it('#hightlight', function () {
+    this.bootstrap.hightlight();
+    expect(this.bootstrap.animate).toEqual(true);
+    jest.runAllTimers();
+    expect(this.bootstrap.animate).toEqual(false);
+  });
+});
---
+++ app/stores/__tests__/RepoStore.test.js
@@ -0,0 +1,43 @@
+import { RepoStore } from '../RepoStore';
+import Repo from '../Repo';
+
+describe('RepoStore', () => {
+  beforeEach(function() {
+    this.bootstrap = new Repo(
+      {
+        username: 'twbs',
+        reponame: 'bootstrap'
+      }
+    );
+    this.react = new Repo(
+      {
+        username: 'facebook',
+        reponame: 'react'
+      }
+    );
+    this.store = new RepoStore();
+  });
+
+  it('#addRepo', function() {
+    this.store.addRepo(this.bootstrap);
+    this.store.addRepo(this.react);
+
+    expect(this.store.repos.length).toEqual(2);
+  });
+
+  it('#removeRepo', function() {
+    this.store.addRepo(this.bootstrap);
+    this.store.addRepo(this.react);
+    this.store.removeRepo(this.react.id);
+    expect(this.store.repos.length).toEqual(1);
+    this.store.removeRepo(this.bootstrap.id);
+    expect(this.store.repos.length).toEqual(0);
+  });
+
+  it('#removeRepo', function() {
+    this.store.addRepo(this.bootstrap);
+    this.store.addRepo(this.react);
+    this.store.removeAllRepos();
+    expect(this.store.repos.length).toEqual(0);
+  });
+});
