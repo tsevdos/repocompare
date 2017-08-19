c258635955a5b84ada3ce548722d7a1c60481036
John Tsevdos <tsevdosjohn@gmail.com>
2017-08-20 00:55:23 +0300

    Lint webpack configuration

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,70 +1,68 @@
-import webpack from 'webpack'
-import HtmlWebpackPlugin from 'html-webpack-plugin'
-import path from 'path'
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from "path";
 
 // Set BABEL_ENV to development or production
-const LAUNCH_COMMAND = process.env.npm_lifecycle_event
-const isProduction = LAUNCH_COMMAND === 'build'
-process.env.BABEL_ENV = isProduction ? 'production' : 'development'
+const LAUNCH_COMMAND = process.env.npm_lifecycle_event;
+const isProduction = LAUNCH_COMMAND === "build";
+process.env.BABEL_ENV = isProduction ? "production" : "development";
 
 // Set up PATHS constants
 const PATHS = {
-  app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'dist')
-}
+  app: path.join(__dirname, "app"),
+  build: path.join(__dirname, "dist")
+};
 
 // Plugins
 // Create an HTML page on root
 const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: PATHS.app + '/index.html',
-  filename: '../index.html',
-  inject: 'body'
-})
+  template: PATHS.app + "/index.html",
+  filename: "../index.html",
+  inject: "body"
+});
 
 // More optimization on bundle.js in production
 const productionPlugin = new webpack.DefinePlugin({
-  'process.env': {
-    NODE_ENV: JSON.stringify('production')
+  "process.env": {
+    NODE_ENV: JSON.stringify("production")
   }
-})
+});
 
 const baseConfig = {
-  entry: [
-    PATHS.app
-  ],
+  entry: [PATHS.app],
   output: {
     path: PATHS.build,
-    filename: 'bundle.js',
-    publicPath: 'dist/'
+    filename: "bundle.js",
+    publicPath: "dist/"
   },
   module: {
     loaders: [
       {
         test: [/\.js$/, /\.jsx$/],
         include: PATHS.app,
-        loader: 'babel'
+        loader: "babel"
       },
       {
         test: /\.json$/,
         include: PATHS.app,
-        loader: 'json'
+        loader: "json"
       },
       {
         test: /\.css$/,
-        loader: 'style!css?modules',
+        loader: "style!css?modules",
         include: /flexboxgrid/
       }
     ]
   },
   resolve: {
-    root: path.resolve('./app'),
-    extensions: ['', '.js', '.jsx', '.json', '.css'],
-    modulesDirectories: ['node_modules']
+    root: path.resolve("./app"),
+    extensions: ["", ".js", ".jsx", ".json", ".css"],
+    modulesDirectories: ["node_modules"]
   }
-}
+};
 
 const developmentConfig = {
-  devtool: 'cheap-module-inline-source-map',
+  devtool: "cheap-module-inline-source-map",
   devServer: {
     hot: true,
     inline: true,
@@ -73,11 +71,15 @@ const developmentConfig = {
     historyApiFallback: true
   },
   plugins: [HTMLWebpackPluginConfig]
-}
+};
 
 const productionConfig = {
-  devtool: 'cheap-module-source-map',
+  devtool: "cheap-module-source-map",
   plugins: [HTMLWebpackPluginConfig, productionPlugin]
-}
+};
 
-export default Object.assign({}, baseConfig, isProduction === true ? productionConfig : developmentConfig)
+export default Object.assign(
+  {},
+  baseConfig,
+  isProduction === true ? productionConfig : developmentConfig
+);
